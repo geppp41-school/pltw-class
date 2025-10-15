e2dbf748753f69bd1fb325baf573e2722c5fdb51
geppp41-school <1118807@apps.nsd.org>
2025-10-15 15:09:48 +0000

    i did some stuff

--- conputer-science-class/PythonGame/Main.py
+++ conputer-science-class/PythonGame/Main.py
@@ -2,4 +2,5 @@ from Screen import Window
 
 window = Window.Window()
 window.bindKey("<Escape>", window.quit)#dont use () or it will call the function even if the key is not pressed
+window.setBackgroundColor((0,0,0))
 window.mainLoop()
