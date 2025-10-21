95129a5e1e3efd395306b7b6a13e903f1e33def0
geppp41-school <1118807@apps.nsd.org>
2025-10-21 20:06:27 +0000

    reseting the codespace

--- computer-science-class/pltw-1.1.9/Main.py
+++ computer-science-class/pltw-1.1.9/Main.py
@@ -1,9 +1,9 @@
-#a119_turtle thing
 import turtle
+import tkinter
 from game.player import Player
 from util.input import input
 from game.EnemyBase import EnemyBase
-
+##TODO: remember to run pip3 install pythonturtle to update turtle
 
 player : Player = Player(turtle.Turtle())
 Input : input = input(player.getScreen())
