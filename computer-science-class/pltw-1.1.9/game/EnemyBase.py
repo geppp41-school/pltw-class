3918d7436af0b358036b985d10247c6fdceda0b0
geppp41-school <1118807@apps.nsd.org>
2025-10-22 01:39:26 +0000

    i did something

--- computer-science-class/pltw-1.1.9/game/EnemyBase.py
+++ computer-science-class/pltw-1.1.9/game/EnemyBase.py
@@ -17,9 +17,10 @@ class EnemyBase:
         self.__frames = {}
         self.__currentFrame = 0
         self.__lastFrameTime = time.time()
-        ##TODO: get the frames of the gif extracted and animate turtle
-        self.__body.getscreen().register_shape("pltw-1.1.9\\assets\\enemies\\GreenSlime\\SlimeMoving.gif")
-        self.__body.shape("pltw-1.1.9\\assets\\enemies\\GreenSlime\\SlimeMoving.gif")
+        #TODO: get the frames of the gif extracted and animate turtle
+
+        self.__body.getscreen().register_shape("computer-science-class/pltw-1.1.9/assets/enemies/GreenSlime/slime_10.png")
+        self.__body.shape("computer-science-class/pltw-1.1.9/assets/enemies/GreenSlime/slime_10.png")
         pass
 
 
