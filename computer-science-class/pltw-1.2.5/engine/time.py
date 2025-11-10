aa68ede313b82fca4fb53f326c749e0b3cde02b1
geppp41-school <1118807@apps.nsd.org>
2025-11-10 21:10:07 +0000

    testing

--- computer-science-class/pltw-1.2.5/engine/time.py
+++ computer-science-class/pltw-1.2.5/engine/time.py
@@ -1,3 +1,12 @@
 import datetime
-
+import ctypes
+from os import wait
+from time import sleep
 #https://stackoverflow.com/questions/10192903/time-in-milliseconds-in-c
+
+time_lib = ctypes.CDLL('./computer-science-class/pltw-1.2.5/engine/time.so')
+time_lib.getTimeMs.restype = ctypes.c_double
+
+print(time_lib.getTimeMs())
+sleep(0.1)
+print(time_lib.getTimeMs())
