bb4a8cebf2fa395c37e68c7eddddc2159d2a2fb5
geppp41-school <1118807@apps.nsd.org>
2025-11-22 18:01:24 +0000

    renamed node_2d.py

--- computer-science-class/pltw-1.2.5-godot/game/src/python/player/main.py
+++ computer-science-class/pltw-1.2.5-godot/game/src/python/player/main.py
@@ -6,26 +6,24 @@ from py4godot.classes.core import Vector3
 from py4godot.classes.Node2D import Node2D
 
 @gdclass
-class node_2d(Node2D):
+class main(Node2D):
 
 	# define properties like this
-	test_int: int = 5
-	test_float: float = 5.2
-	test_bool: bool = True
-	test_vector: Vector3 = Vector3.new3(1,2,3)
 
 	# define signals like this
-	test_signal = signal([SignalArg("test_arg", int)])
+	
 
 
 	def _ready(self) -> None:
+		
 		pass
 		# put initialization code here
 
 	def _process(self, delta:float) -> None:
 		pass
 		# put dynamic code here
-
+	
+	
 	# Hide the method in the godot editor
 	@private
 	def test_method(self):
