952b7a2e4acd7759c8eb5ad28532897358dc1d4e
geppp41-school <1118807@apps.nsd.org>
2025-12-01 16:40:36 +0000

    slime now moves towards the player

--- computer-science-class/pltw-1.2.5-godot/game/src/python/enemy/slime/slime.py
+++ computer-science-class/pltw-1.2.5-godot/game/src/python/enemy/slime/slime.py
@@ -2,7 +2,7 @@
 from py4godot.methods import private
 from py4godot.signals import signal, SignalArg
 from py4godot.classes import gdclass
-from py4godot.classes.core import Vector3
+from py4godot.classes.core import Vector2, Vector3
 from py4godot.classes.Node2D import Node2D
 from py4godot.classes.AnimatedSprite2D import AnimatedSprite2D
 
@@ -11,19 +11,32 @@ class slime(Node2D):
 
 	# define properties like this
 	animated_body:  AnimatedSprite2D = None # type: ignore
+	target: Node2D = None  # type: ignore
 
 	# define signals like this
 	test_signal = signal([SignalArg("test_arg", int)])
 
 
 	def _ready(self) -> None:
-		animated_body = self.get_node("AnimatedSprite2D")  # type: ignore
+		self.animated_body = self.get_node("AnimatedSprite2D")  # type: ignore
+		self.animated_body.play("Moving")
+		if(self.get_parent() != None):
+			self.target = self.get_parent().get_node("Player")  # type: ignore
+		self.position = Vector2.new3(100,100)
 		pass
 		# put initialization code here
 
 	def _process(self, delta:float) -> None:
 		pass
 		# put dynamic code here
+
+	def _physics_process(self, delta: float) -> None:
+		if self.target != None:
+			angle = self.position.angle_to_point(self.target.position) # get angle to player
+			direction = Vector2.RIGHT.rotated(angle)# get direction to player
+			velocity = direction * 16 # sets speed
+			self.position += velocity*delta #moves position depending on time between frames or delta 
+		pass
 
 	# Hide the method in the godot editor
 	@private
