e4bc5f426619c9db47009140f3327d7e54c695d6
Ethan Gepner <1118807@apps.nsd.org>
2025-12-02 09:39:41 -0800

    slime now only moves while jumping

--- computer-science-class/pltw-1.2.5-godot/game/src/python/enemy/slime/slime.py
+++ computer-science-class/pltw-1.2.5-godot/game/src/python/enemy/slime/slime.py
@@ -12,9 +12,11 @@ class slime(Node2D):
 	# define properties like this
 	animated_body:  AnimatedSprite2D = None # type: ignore
 	target: Node2D = None  # type: ignore
-
+	speed: int = 30
+	_can_move:bool = True
+	target_angle:float = 0
 	# define signals like this
-	test_signal = signal([SignalArg("test_arg", int)])
+
 
 
 	def _ready(self) -> None:
@@ -27,17 +29,36 @@ class slime(Node2D):
 		# put initialization code here
 
 	def _process(self, delta:float) -> None:
-		pass
+		
 		# put dynamic code here
+		pass
 
 	def _physics_process(self, delta: float) -> None:
-		if self.target != None:
-			angle = self.position.angle_to_point(self.target.position) # get angle to player
-			direction = Vector2.RIGHT.rotated(angle)# get direction to player
-			velocity = direction * 16 # sets speed
-			self.position += velocity*delta #moves position depending on time between frames or delta 
-		pass
+		#check if there is a target
+		#check if the slime can mvoe
+		#check if the slime is moving
+		if(self.animated_body.frame == 0 or self.animated_body.frame >= 6):
+			self._can_move = False
+			self.target_angle = self.position.angle_to_point(self.target.position)
+		else:
+			self._can_move = True
+			
+		
+		if(self.has_target() and self.can_move()):
+			move_direction = Vector2.RIGHT.rotated(self.target_angle)
+			velocity = move_direction * self.speed
+			self.position += velocity * delta
+		# if self.target != None and self._can_move:
+		# 	angle = self.position.angle_to_point(self.target.position) # get angle to player
+		# 	direction = Vector2.RIGHT.rotated(angle)# get direction to player
+		# 	velocity = direction * self.speed # sets speed
+		# 	self.position += velocity*delta #moves position depending on time between frames or delta 
+		# pass
 
+	def has_target(self) -> bool:
+		return self.target != None
+	def can_move(self) -> bool:
+		return self._can_move
 	# Hide the method in the godot editor
 	@private
 	def test_method(self):
