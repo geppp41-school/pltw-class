8c3e2698fc3feee5bbe179b825c474060ba1ff23
geppp41-school <1118807@apps.nsd.org>
2025-12-08 20:57:12 +0000

    created a start screen

--- computer-science-class/pltw-1.2.5-godot/game/src/python/game.py
+++ computer-science-class/pltw-1.2.5-godot/game/src/python/game.py
@@ -1,24 +1,42 @@
+from py4godot.classes.InputEvent import InputEvent
+from py4godot.classes.ResourceLoader import ResourceLoader
 from py4godot.methods import private
 from py4godot.classes import gdclass
 from py4godot.classes.Node import Node
-import src.python.registry as registry
+
 @gdclass
 class game(Node):
 
 	# define properties like this
 	# define signals like this
-
+	player = None
+	start_screen:Node = Node.new()
 
 	def _ready(self) -> None:
-		registry.register("slime", "res://scene/enemy/slime/slime.tscn")
+		self.player = ResourceLoader.instance().load("res://scene/start_screen.tscn").instantiate()
+		self.start_screen = ResourceLoader.instance().load("res://scene/start_screen.tscn").instantiate()
+		self.add_child(self.start_screen)
+		#self.get_node("start_screen").get_node("play_button").connect("pressed", self.play)
+		self.play_button = self.get_node("start_screen").get_node("play_button")
+		self.play_button.pressed.connect(self.play)
 		
-		registry.register("player", "res://scene/Player/player.tscn")
+		
+		
+		
 		pass
 		# put initialization code here
 
 	def _process(self, delta:float) -> None:
 		pass
 		# put dynamic code here
+
+	def _input(self, event: InputEvent) -> None:
+		return super()._input(event)
+	
+	def play(self):
+		self.start_screen.queue_free()
+		print("Play was pressed")
+		pass
 
 	# Hide the method in the godot editor
 	@private
